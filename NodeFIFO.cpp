27f0122eef71b3c2126164633acf990e25ccebe3
Eduard18rus <eduard.karpov.83@gmail.com>
2023-07-23 17:14:50 +0400

    FIFO
    FIFO

---
+++ NodeFIFO.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include "List.h"
+
+/* Добавить в односвязный* список возможность
+ добавлять другой список и не забывайте отобрать права на указатели!*/
+
+using namespace std;
+
+int main() {
+
+	List list;
+	list.push_back(1);
+	list.push_back(2);
+	list.push_back(3);
+	list.push_back(4);
+	list.push_back(5);
+	
+
+	cout << "PRINT" << endl;
+	list.print();
+
+	cout << "\nPOP" << endl;
+	while (!list.empty()) {
+		cout << list.pop() << endl;
+	}
+
+	return 0;
+}
